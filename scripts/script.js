0550abc54899147be2950920dc47c3031d40d977
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-11-15 11:26:36 +0300

    Бонусная программа (есть карта) стили

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 $(".bonus-programm__content button").click(function () {
   $(".bonus-programm__personal").addClass("bonus-programm__personal--visible");
 });
+
+$(".bonus-programm--login .bonus-card__form .reset").click(function () {
+  $(".bonus-programm__personal").removeClass("bonus-programm__personal--visible");
+});
