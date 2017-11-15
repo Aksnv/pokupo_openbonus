3d11171bb2fed4723717489284c7f38ce57979a9
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-11-15 13:53:25 +0300

    Оформление заказа

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/* OpenBonus */
+
 $(".bonus-programm__content button").click(function () {
   $(".bonus-programm__personal").addClass("bonus-programm__personal--visible");
 });
@@ -5,3 +7,5 @@ $(".bonus-programm__content button").click(function () {
 $(".bonus-programm--login .bonus-card__form .reset").click(function () {
   $(".bonus-programm__personal").removeClass("bonus-programm__personal--visible");
 });
+
+/* END - OpenBonus */
